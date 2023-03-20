f12211a95be28ed8ac27f44115cb28b03760529c
Andrei-Carlo Papuc <andrei-carlo@hubs.com>
2023-03-20 15:06:06 +0100

    init commit, setting environments and some stuff up

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mpc_drone
 MPC path follower for a drone 
+
+
+## Environment
+To build the environment run
+
+`conda env create -f environment.yml`
---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+if __name__ == "__main__":
+    # tuples of x, y, z
+    target_path = [(0.0, 0.0, 0.0), (1.0, 0.0, 0.0), (1.0, 1.0, 0.0), (1.0, 1.0, 1.0)]
